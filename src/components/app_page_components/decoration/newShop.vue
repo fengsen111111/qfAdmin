07421f321235d7102e28303556787f08114121ed
fengsen111111 <2072958590@qq.com>
2025-06-17 11:06:20 +0800

    2025年6月16日14点16分

--- src/components/app_page_components/decoration/newShop.vue
+++ src/components/app_page_components/decoration/newShop.vue
@@ -33,6 +33,12 @@
                   return {
                     value: y.id,
                     label: y.name,
+                    children: y.children.length == 0 ? [] : y.children.map((z) => {
+                      return {
+                        value: z.id,
+                        label: z.name,
+                      }
+                    })
                   }
                 })
               }
@@ -48,18 +54,13 @@
     twoClassify: '',
     twoList: [],
     threeClassify: '',
+    threeList: [],
+    fourClassify: '',
   })
   const selClassify = ref('')//已选分类名字
-  const isXn = ref(false)//是否是虚拟类目
   function onChange(val, label) {
     const path = findCategoryPath(treeData.value, val);
-    // console.log('找到上级数据', path);
     selClassify.value = ''//清空
-    if (path[0].label == '虚拟商品') {
-      isXn.value = true
-    } else {
-      isXn.value = false
-    }
     if (path.length > 0) {
       checkClassify.value.oneClassify = path[0].value
       checkClassify.value.oneList = path[0].children
@@ -72,10 +73,13 @@
     }
     if (path.length > 2) {
       checkClassify.value.threeClassify = path[2].value
+      checkClassify.value.threeList = path[2].children
       selClassify.value = path[0].label + ' > ' + path[1].label + ' > ' + path[2].label
     }
-    // console.log('typeVal',typeVal.value);
-
+    if (path.length > 3) {
+      checkClassify.value.fourClassify = path[3].value
+      selClassify.value = path[0].label + ' > ' + path[1].label + ' > ' + path[2].label + ' > ' + path[3].label
+    }
   }
   function findCategoryPath(data, val, path = []) {
     for (const category of data) {
@@ -89,7 +93,6 @@
     }
     return null; // 未找到
   }
-
   // 
   function editData(index, item) {
     if (index == 1) {
@@ -101,18 +104,13 @@
       checkClassify.value.twoList = item.children
     } else if (index == 3) {
       checkClassify.value.threeClassify = item.value
+      checkClassify.value.threeList = item.children
+    } else if (index == 4) {
+      checkClassify.value.fourClassify = item.value
     }
     typeVal.value = item.value
-    // console.log('点击的级别',index);
-    // console.log('点击的数据',item);
     const path = findCategoryPath(treeData.value, item.value);
-    // console.log('上级数据',path);
     selClassify.value = ''//清空
-    if (path[0].label == '虚拟商品') {
-      isXn.value = true
-    } else {
-      isXn.value = false
-    }
     if (path.length > 0) {
       selClassify.value = path[0].label
     }
@@ -122,30 +120,24 @@
     if (path.length > 2) {
       selClassify.value = path[0].label + ' > ' + path[1].label + ' > ' + path[2].label
     }
+    if (path.length > 3) {
+      selClassify.value = path[0].label + ' > ' + path[1].label + ' > ' + path[2].label + ' > ' + path[3].label
+    }
   }
-
-  import QRCode from 'qrcode';
 
   let arrType = ref([])//缓存的分类
   arrType.value = localStorage.getItem('hcspfl') ? JSON.parse(localStorage.getItem('hcspfl')).slice(0, 4) : []
-  // console.log('缓存的分类', arrType.value);
-
-  const money_log_id = ref('')//支付日志id,查询支付结果
 
   const typeVis = ref(1)//1选分类 2 填商品信息
 
   // 发布该类商品2
   function handFb() {
-    console.log('checkClassify.value.threeClassify', checkClassify.value);
-    console.log('pageData.data', pageData.data);
     pageData.data = pageData.data ? pageData.data : {}
-    // pageData.data.typeId = checkClassify.value.threeClassify
-    pageData.data.typeId = [checkClassify.value.oneClassify, checkClassify.value.twoClassify, checkClassify.value.threeClassify]
+    pageData.data.typeId = [checkClassify.value.oneClassify, checkClassify.value.twoClassify, checkClassify.value.threeClassify, checkClassify.value.fourClassify]
     pageData.data.typeName = selClassify.value
     let arr = selClassify.value.split(' > ')
-    console.log('arr', arr, checkClassify.value.threeClassify);
-    if (arr.length != 3) {
-      message.error('请选择三级分类！');
+    if (arr.length != 4) {
+      message.error('请选择四级分类！');
       return false
     }
     arrType.value.unshift(selClassify.value)
@@ -155,7 +147,6 @@
   }
   // 点击缓存的分类
   function cliHcType(str) {
-    // console.log('点击缓存的分类', str);
     let arr = str.split(' > ')
     treeData.value.map((one) => {
       if (one.label == arr[0]) {
@@ -164,13 +155,7 @@
             two.children.map((three) => {
               if (three.label == arr[2]) {
                 const path = findCategoryPath(treeData.value, three.value);
-                // console.log('path',path);
                 selClassify.value = ''//清空
-                if (path[0].label == '虚拟商品') {
-                  isXn.value = true
-                } else {
-                  isXn.value = false
-                }
                 if (path.length > 0) {
                   checkClassify.value.oneClassify = path[0].value
                   checkClassify.value.oneList = path[0].children
@@ -184,6 +169,9 @@
                 if (path.length > 2) {
                   checkClassify.value.threeClassify = path[2].value
                   selClassify.value = path[0].label + ' > ' + path[1].label + ' > ' + path[2].label
+                }
+                if (path.length > 3) {
+                  selClassify.value = path[0].label + ' > ' + path[1].label + ' > ' + path[2].label + ' > ' + path[3].label
                 }
               }
             })
@@ -225,33 +213,6 @@
     global.router.push(url)
   }
   const spinning = ref(false)//加载
-  const isPay = ref(false)//弹窗
-  let qrCodeData = ref('')//存储生成的二维码数据URL
-  function handOKCode() {
-    console.log('确定');
-    // 查询支付结果
-    global.axios
-      .post('decoration/Store/payTypePricesResult', {
-        money_log_id: money_log_id.value
-      }, global)
-      .then((res) => {
-        console.log('查询支付结果', res);
-        // P支付中 S成功 F失败  
-        if (res.result == 'P') {
-          message.error('支付中')
-        } else if (res.result == 'S') {
-          message.success('支付成功')
-          isPay.value = false
-          setTimeout(() => {
-            typeVis.value = 2
-          }, 2000);
-        } else if (res.result == 'F') {
-          message.error('支付失败')
-        } else {
-          message.error('未知')
-        }
-      })
-  }
   // 子组件的修改分类
   function editType() {
     typeVis.value = 1
@@ -261,10 +222,13 @@
     console.log('去店铺详情');
     emit('toShopDetails')
   }
-
   // 去运费模板
   function _djtzmk() {
     emit("djtzmk", '快递物流', '运费模板');
+  }
+  function handleClick() {
+    console.log('111');
+
   }
 </script>
 
@@ -292,7 +256,7 @@
         </div>
         <div class="a8">
           <div>选择分类</div>
-          <div class="a9">商品分类请选到三级</div>
+          <div class="a9">商品分类请选到四级</div>
         </div>
         <div style="margin-top: 10px;">
           <a-tree-select @change="onChange" v-model:value="typeVal" show-search style="width: 100%;"
@@ -322,12 +286,21 @@
               <RightOutlined style="color: #999999;" />
             </div>
           </div>
-          <div>
+          <div class="a12">
             <div v-if="checkClassify.twoList.length==0">
               <a-empty style="margin-top: 10%;" />
             </div>
             <div @click="editData(3,item)" :class="item.value==checkClassify.threeClassify?'checked_one_type':''"
               v-for="item in checkClassify.twoList" :key="item.value" class="a14">
+              {{item.label}}
+            </div>
+          </div>
+          <div>
+            <div v-if="checkClassify.threeList.length==0">
+              <a-empty style="margin-top: 10%;" />
+            </div>
+            <div @click="editData(4,item)" :class="item.value==checkClassify.fourClassify?'checked_one_type':''"
+              v-for="item in checkClassify.threeList" :key="item.value" class="a14">
               {{item.label}}
             </div>
           </div>
@@ -337,7 +310,6 @@
           <div>已选分类</div>
           <div style="margin-left: 20px;">
             <div>{{selClassify}}</div>
-            <!-- <div v-if="isXn" style="color: #FF7300;"> 本类目为虚拟商品类目，请勿发布实物商品 </div> -->
           </div>
         </div>
       </div>
@@ -350,14 +322,6 @@
       <publishPage :pageData="pageData" @closeChildPageTwo="closeChildPageTwo" @closeChildPage="closeChildPage"
         @editType="editType" @openChildPage="openChildPage" @toShopInfo="toShopInfo" @djtzmk="_djtzmk" />
     </div>
-    <!-- 支付弹框 -->
-    <a-modal v-model:visible="isPay" :centered="true" @ok="handOKCode" :keyboard="false" title="支付二维码" ok-text="已支付"
-      cancel-text="放弃" :maskClosable="false">
-      <div class="a18">
-        <div>请打开支付宝扫描二维码！</div>
-        <img :src="qrCodeData" alt="支付二维码" />
-      </div>
-    </a-modal>
   </a-spin>
 </template>
 
@@ -436,7 +400,7 @@
     margin-top: 10px;
     border: 1px solid #d9d9d9;
     display: grid;
-    grid-template-columns: repeat(3, minmax(0, 1fr));
+    grid-template-columns: repeat(4, minmax(0, 1fr));
     border-radius: 4px;
   }
 
