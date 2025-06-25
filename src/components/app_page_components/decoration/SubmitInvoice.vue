a558067fa103550c497ceef80a23868168516700
fengsen111111 <2072958590@qq.com>
2025-06-25 14:28:59 +0800

    2025年6月25日13点49分

--- src/components/app_page_components/decoration/SubmitInvoice.vue
+++ src/components/app_page_components/decoration/SubmitInvoice.vue
@@ -26,16 +26,89 @@
 			},
 		});
 	}
+	// 商家提交开票
+	function _submitInvoice(item) {
+		global.axios
+			.post('decoration/Invoices/submitInvoice', {
+				type: 'other',//all全部 other部分  
+				money_log_ids: item.id,//选中的要开票的服务日志ID type为全部时传空
+			}, global)
+			.then((res) => {
+				console.log('商家提交开票', res);
+			});
+	}
+	// 全部开票
+	function allKp(){
+		global.axios
+			.post('decoration/Invoices/submitInvoice', {
+				type: 'all',//all全部 other部分  
+				money_log_ids: "",//选中的要开票的服务日志ID type为全部时传空
+			}, global)
+			.then((res) => {
+				console.log('商家提交开票', res);
+			});
+	}
 
-	// f商家服务费日志a
+	// 商家服务费日志
 	function getStoreMoneyLogList() {
 		global.axios
 			.post('decoration/MoneyLog/getStoreMoneyLogList', {}, global)
 			.then((res) => {
-				console.log('f商家服务费日志a', res);
+				console.log('商家服务费日志', res);
+				dataSource.value = res.list
 			});
 	}
 	getStoreMoneyLogList()
+
+	const dataSource = ref([
+		// {
+		// 	id: '1',
+		// 	create_time: '2022-12-12 12:12:12',
+		// 	type: 'j',
+		// 	trans_amt: '10.00',
+		// 	has_invoice: 'Y',
+		// },
+		// {
+		// 	id: '2',
+		// 	create_time: '2022-12-12 12:12:12',
+		// 	type: 'q',
+		// 	trans_amt: '10.00',
+		// 	has_invoice: 'N',
+		// },
+	])
+
+	const columns = [
+		{
+			title: '操作',
+			dataIndex: 'action',
+			key: 'action',
+		},
+		{
+			title: '#',
+			dataIndex: 'id',
+			key: 'id',
+		},
+		{
+			title: '类型',
+			dataIndex: 'type',
+			key: 'type',
+		},
+		{
+			title: '是否可以开发票',
+			dataIndex: 'has_invoice',
+			key: 'has_invoice',
+		},
+		{
+			title: '发生时间',
+			dataIndex: 'create_time',
+			key: 'create_time',
+		},
+		{
+			title: '金额',
+			dataIndex: 'trans_amt',
+			key: 'trans_amt',
+		},
+	]
 </script>
 
 <template>
@@ -48,7 +121,27 @@
 			<div style="font-size: 18px;">发票</div>
 		</div>
 		<div>
-			21312312312
+			<a-popconfirm title="确定要执行操作吗?" @confirm="allKp" >
+				<a-button class="editable-add-btn" style="margin-bottom: 8px">全部开票</a-button>
+			</a-popconfirm>
+			<a-table :dataSource="dataSource" :columns="columns">
+				<template #bodyCell="{ column, record }">
+					<template v-if="column.key == 'type'">
+						<span v-if="record.type=='j'">购买曝光量</span>
+						<span v-else-if="record.type=='q'">订单服务费</span>
+					</template>
+					<template v-else-if="column.key == 'has_invoice'">
+						<span v-if="record.has_invoice=='Y'" style="color: green;">可以</span>
+						<span v-else-if="record.has_invoice=='N'" style="color: red;">不可以</span>
+					</template>
+					<template v-else-if="column.key == 'action'">
+						<a-popconfirm title="确定要执行操作吗?" @confirm="_submitInvoice(record)">
+							<span v-if="record.has_invoice=='Y'" style="cursor: pointer;color: #1890FF;">开票</span>
+						</a-popconfirm>
+
+					</template>
+				</template>
+			</a-table>
 		</div>
 	</div>
 </template>
